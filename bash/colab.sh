20a82d07e73c251c08165c136c672da2e09ad036
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 05:15:10 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -19,7 +19,7 @@
 
 
 
-#!/bin/bash
+#xxxx/bin/bash
 # KoboldAI Easy Colab Deployment Script by Henk717
 
 # read the options
@@ -66,7 +66,6 @@ while true ; do
     esac
 done
 
-# Assigning the model directly in the script
 model="--model TehVenom/Pygmalion-13b-Merged"
 
 # Create the Launch function so we can run KoboldAI at different places in the script
@@ -79,6 +78,7 @@ function launch
     else
     cd /content/KoboldAI-Client
     echo "Launching KoboldAI with the following options : python3 aiserver.py$model$kmpath$configname$ngrok$localtunnel$savemodel$revision --colab"
+    #python3 aiserver.py$model$kmpath$configname$ngrok$localtunnel$savemodel$revision --colab
     python3 aiserver.py $model $kmpath $configname $ngrok $localtunnel $savemodel $revision --colab
     exit
     fi
@@ -188,23 +188,64 @@ if [ "$init" != "skip" ]; then
     rm themes
     rm -rf themes/
     ln -s /content/drive/MyDrive/KoboldAI/stories/ stories
-    ln -s /content/drive/MyDrive/KoboldAI/userscripts/ userscripts
+    ln -s /content/drive/MyDrive/KoboldAI/settings/ settings
     ln -s /content/drive/MyDrive/KoboldAI/softprompts/ softprompts
+    ln -s /content/drive/MyDrive/KoboldAI/userscripts/ userscripts
+    ln -s /content/drive/MyDrive/KoboldAI/models/ models
     ln -s /content/drive/MyDrive/KoboldAI/presets/ presets
     ln -s /content/drive/MyDrive/KoboldAI/themes/ themes
 
-    if [[ "$download" != "skip" ]]; then
-        python3 /content/KoboldAI-Client/downloader.py $download --savepath $dloc $aria2
-        python3 /content/KoboldAI-Client/unpack.py $dloc $z7 $tar
-        echo KoboldAI downloaded and installed!
-        launch
+    if [ -n "${COLAB_TPU_ADDR+set}" ]; then
+        pip install -r requirements_mtj.txt
     else
-        echo KoboldAI downloaded and installed!
-        launch
+        pip install -r requirements.txt
     fi
+    
+    # Make sure Colab has the system dependencies
+    sudo apt install netbase aria2 -y
+    npm install -g localtunnel
+fi
 
-else
-    echo Launching KoboldAI...
+cd /content
+
+# Models extracted? Then we skip anything beyond this point for faster loading.
+if [ -f "/content/extracted" ]; then
     launch
 fi
 
+# Is the model extracted on Google Drive? Skip the download and extraction
+# Only on Google Drive since it has a big impact there if we don't, and locally we have better checks in place
+if [ "$xloc" == "/content/drive/MyDrive/KoboldAI/models/"  ] && [[ -d $mpath ]];then
+    launch
+fi
+
+#Download routine for regular Downloads
+if [ ! -z ${download+x} ]; then
+    wget -c $download -P $dloc
+fi
+
+#Download routine for Aria2c scripts
+if [ ! -z ${aria2+x} ]; then
+    curl -L $aria2 | aria2c -x 10 -s 10 -j 10 -c -i- -d$dloc --user-agent=KoboldAI --file-allocation=none
+fi
+
+#Extract the model with 7z
+if [ ! -z ${z7+x} ]; then
+    7z x -o$xloc $dloc/$z7 -aos
+    touch /content/extracted
+fi
+
+#Extract the model in a ZSTD Tar file
+if [ ! -z ${tar+x} ]; then
+    git clone https://github.com/VE-FORBRYDERNE/pv
+    cd pv
+    ./configure
+    make
+    make install
+    cd ..
+    apt install zstd -y
+    pv $dloc/$tar | tar -I zstd -C $xloc -x
+    touch /content/extracted
+fi
+
+launch
