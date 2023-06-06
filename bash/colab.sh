32d391bc18f18dc3beeb172b416338f1a1326ef9
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 04:23:38 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # Display NVIDIA GPU information
-nvidia-smi
+#nvidia-smi
 
 # Clone the GitHub repository for text generation web UI
-git clone https://github.com/oobabooga/text-generation-webui
+#git clone https://github.com/oobabooga/text-generation-webui
 
 # Change the current directory to text-generation-webui
-cd text-generation-webui
+#cd text-generation-webui
 
 # Install the required Python packages from the requirements.txt file
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Download the pre-trained model for text generation
-python download-model.py TehVenom/Pygmalion-13b-Merged
+#python download-model.py TehVenom/Pygmalion-13b-Merged
 
 # Start the server by running server.py and enable sharing
-python server.py --share --api
+#python server.py --share --api
+
+Model = "TehVenom/Pygmalion-13b-Merged"
