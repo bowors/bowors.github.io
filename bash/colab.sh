78e2ae6e500a0598d5df6eb9ca395bc20db594c1
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 00:49:36 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Display NVIDIA GPU information
+nvidia-smi
+
+# Clone the GitHub repository for text generation web UI
 git clone https://github.com/oobabooga/text-generation-webui
+
+# Change the current directory to text-generation-webui
 cd text-generation-webui
+
+# Install the required Python packages from the requirements.txt file
 pip install -r requirements.txt
-python server.py --share
+
+# Start the server by running server.py and enable sharing
+python server.py --share --api --public-api --api-blocking-port 8181 ----api-streaming-port 8182
