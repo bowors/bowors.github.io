19791a487ffae7b62b12edf9ff4627cedaa5e6a2
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 01:23:13 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Display NVIDIA GPU information
-!nvidia-smi
+nvidia-smi
 
 # Clone the GitHub repository for text generation web UI
 git clone https://github.com/oobabooga/text-generation-webui
