ac283e587d35c9eecf4d33a630f9bd9b4ee119b2
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 18:12:55 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -1,69 +1,43 @@
 #!/bin/bash
-# Display NVIDIA GPU information
-#nvidia-smi
 
-# Clone the GitHub repository for text generation web UI
-#git clone https://github.com/oobabooga/text-generation-webui
+LOG_FILE="script.log"
+
+# Function to log messages to the log file
+log() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+}
+
+# Display NVIDIA GPU information
+nvidia-smi
+
+# Clone the GitHub repository for text generation web UI if it doesn't exist
+if [ ! -d "text-generation-webui" ]; then
+  log "Cloning text-generation-webui repository..."
+  git clone https://github.com/oobabooga/text-generation-webui || { log "Error: Failed to clone repository."; exit 1; }
+fi
 
 # Change the current directory to text-generation-webui
-#cd text-generation-webui
+log "Changing directory to text-generation-webui..."
+cd text-generation-webui || { log "Error: Failed to change directory."; exit 1; }
 
-# Install the required Python packages from the requirements.txt file
-#pip install -r requirements.txt
+# Install the required Python packages from the requirements.txt file if not already installed
+log "Installing Python dependencies..."
+if ! command -v pip >/dev/null 2>&1; then
+  log "Error: pip command not found."; exit 1;
+fi
 
-# Download the pre-trained model for text generation
-#python download-model.py TehVenom/Pygmalion-13b-Merged
+if [ ! -f "requirements.txt" ]; then
+  log "Error: requirements.txt file not found."; exit 1;
+fi
+
+pip install -r requirements.txt || { log "Error: Failed to install Python dependencies."; exit 1; }
+
+# Download the pre-trained model for text generation if not already downloaded
+log "Downloading pre-trained model..."
+python download-model.py TehVenom/Pygmalion-13b-Merged || { log "Error: Failed to download pre-trained model."; exit 1; }
 
 # Start the server by running server.py and enable sharing
-#python server.py --share --api
+log "Starting the server..."
+nohup python server.py --share --api >> "$LOG_FILE" 2>&1 &
+log "Server started. Check $LOG_FILE for server logs."
 
-
-
-#xxxx/bin/bash
-# KoboldAI Easy Colab Deployment Script by Henk717
-
-# read the options
-TEMP=`getopt -o m:i:p:c:d:x:a:l:z:g:t:n:b:s:r: --long model:,init:,path:,configname:,download:,aria2:,dloc:,xloc:,7z:,git:,tar:,ngrok:,branch:,savemodel:,localtunnel:,lt:,revision: -- "$@"`
-eval set -- "$TEMP"
-
-# extract options and their arguments into variables.
-while true ; do
-    case "$1" in
-        -m|--model)
-            model=" --model $2" ; shift 2 ;;
-        -r|--revision)
-            revision=" --revision $2" ; shift 2 ;;
-        -i|--init)
-            init=$2 ; shift 2 ;;
-        -p|--path)
-            mpath="$2" ; shift 2 ;;
-        -c|--configname)
-            configname=" --configname $2" ; shift 2 ;;
-        -n|--ngrok)
-            ngrok=" --ngrok" ; shift 2 ;;
-        --lt|--localtunnel)
-            localtunnel=" --localtunnel" ; shift 2 ;;
-        -d|--download)
-            download="$2" ; shift 2 ;;
-        -a|--aria2)
-            aria2="$2" ; shift 2 ;;
-        -l|--dloc)
-            dloc="$2" ; shift 2 ;;
-        -x|--xloc)
-            xloc="$2" ; shift 2 ;;
-        -z|--7z)
-            z7="$2" ; shift 2 ;;
-        -t|--tar)
-            tar="$2" ; shift 2 ;;
-        -g|--git)
-            git="$2" ; shift 2 ;;
-        -b|--branch)
-            branch="$2" ; shift 2 ;;
-        -s|--savemodel)
-            savemodel=" --savemodel" ; shift 2 ;;
-        --) shift ; break ;;
-        *) echo "Internal error!" ; exit 1 ;;
-    esac
-done
-
-set | column -t
