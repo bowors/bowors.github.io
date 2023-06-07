09892bb3dfb8b7ef7d91418a7e2a9d42e45060f8
bowors <24736574+bowors@users.noreply.github.com>
2023-06-08 02:14:37 +0700

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -30,13 +30,14 @@ https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/tok
 https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/tokenizer_config.json" | xargs -n 1 aria2c -x 16 -s 16 --max-connection-per-server=16 --split=16 --min-split-size=1M --max-overall-download-limit=0 --max-download-limit=0 --auto-file-renaming=false -d /content/text-generation-webui/models/Pygmalion-7b-Merged-Safetensors
 # This command uses echo to print the URLs, pipes them to xargs to pass each URL as an argument to aria2c, and downloads the files using specified options.
 
-# Install the required Python packages from the requirements.txt file
-pip install -r requirements.txt
-# This command installs the Python packages listed in the requirements.txt file.
 
 # Change the current directory to text-generation-webui
 cd text-generation-webui
 # This command changes the current directory to the specified directory.
+
+# Install the required Python packages from the requirements.txt file
+pip install -r requirements.txt
+# This command installs the Python packages listed in the requirements.txt file.
 
 # Start the server by running server.py and enable sharing
 python server.py --share --api --public-api
