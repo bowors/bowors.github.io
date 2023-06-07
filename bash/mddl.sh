75ef0e7fe435a7517d33c0b0768d65c454925dd3
bowors <24736574+bowors@users.noreply.github.com>
2023-06-07 22:10:35 +0700

    Update mddl.sh

---
+++ bash/mddl.sh
@@ -0,0 +1,19 @@
+#!apt-get install aria2
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/config.json
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/generation_config.json
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00001-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00002-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00003-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00004-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00005-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00006-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model-00007-of-00007.safetensors
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/model.safetensors.index.json
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/special_tokens_map.json
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/tokenizer.json
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/tokenizer.model
+#https://huggingface.co/TehVenom/Pygmalion-7b-Merged-Safetensors/resolve/main/tokenizer_config.json
+
+pwd
+cd gdrive
+pwd
